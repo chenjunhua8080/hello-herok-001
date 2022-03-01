d94b0a36fad63d62b406d5f38bb486724398be9d
AllenChen <allen.chen@imanagesystems.com>
2022-03-01 11:25:23 +0800

    add stg

--- src/main/java/com/example/helloherok001/HelloController.java
+++ src/main/java/com/example/helloherok001/HelloController.java
@@ -2,16 +2,20 @@ package com.example.helloherok001;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${hello}")
+    private String hello;
+
     @GetMapping("/test/001")
     public Map<String, Object> test001() {
         HashMap<String, Object> map = new HashMap<>();
-        map.put("data", "hello heroku!");
+        map.put("data", "hello heroku! - " + hello);
         return map;
     }
 }
